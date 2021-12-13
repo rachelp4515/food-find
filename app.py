47c4d8a89abf3f533eeb1de0289cf47e5f3be2c9
rachelp4515 <rachel.porhammer@students.dominican.edu>
2021-12-12 22:25:30 -0500

    final touches and fixes. complete project.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from bson.objectid import ObjectId
 import random
 import os
+import datetime
 app = Flask(__name__)
 
 from pymongo import MongoClient
@@ -13,6 +14,7 @@ print(app.config["MONGO_URI"])
 client = MongoClient(app.config["MONGO_URI"])
 db = client.get_database('food-find')
 places = db.places
+comments = db.comments
 
 
 
@@ -44,7 +46,8 @@ def add_place():
 @app.route('/places/<place_id>')
 def show_place(place_id):
     place = places.find_one({'_id': ObjectId(place_id)})
-    return render_template('show_place.html', place = place)
+    place_comments = list(comments.find({'place_id': ObjectId(place_id)})) #listen i know this list thing is gross but it works
+    return render_template('show_place.html', place = place, comments=place_comments)
 
 #---------------------- / edit it
 @app.route('/places/<place_id>/edit')
@@ -81,19 +84,27 @@ def delete_place(place_id):
 @app.route('/places/select')
 def show_selection():
     selected = places.aggregate( [ { "$sample": { "size": 1 } } ])
+    # aggregate is cool! when i talked about it in class i realized i didnt have a thorough 
+    # understanding of it so i looked into it. Aggregate means to collect related items of content 
+    # so as to display or link to them. For the size of my collection (>100), $sample reads all 
+    # the documents that are output from a  preceding aggregation stage or a collection scan and 
+    # then performs a random sort to select however many documents specified. 
     return render_template("show_selection.html", selected=selected)
 
-
 #---------------------- / place description
-# @app.route('/places/descriptions', methods=['POST'])
-# def desc_new():
-#     desc = {
-#         'place_id':ObjectId(request.form.get('place_id')),
-#         'desc-content': request.form.get('desc-content'),
-#     }
-#     descriptions.insert_one(desc) 
-#     return redirect(url_for('places_show', place_id=request.form.get('place_id')))
+@app.route('/places/comments', methods=['POST'])
+def comments_new():
+    comment = {
+        'place_id': ObjectId(request.form.get('place_id')),
+        'comment': request.form.get('comment'),
+        'created_date': datetime.datetime.now()
+    }
+    comments.insert_one(comment) 
+    return redirect(url_for('show_place', place_id=request.form.get('place_id')))
 
+
+# 61b0d596208b7e2e5aa87626
+# 61b0d596208b7e2e5aa87626
 
 
 
